c4566f9cdbb228904a94a5826d00148d3ad904c9
zhenqihu <zhenqi_hu@outlook.com>
2023-04-12 20:07:53 +0800

    finish hw07

--- hw/hw07/hw07.scm
+++ hw/hw07/hw07.scm
@@ -1,11 +1,21 @@
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cdr (cdr s))))
 
-(define (ascending? asc-lst) 'YOUR-CODE-HERE)
+(define (ascending? asc-lst) 
+    (cond ((null? (cdr asc-lst)) #t)
+          ((> (car asc-lst) (cadr asc-lst)) #f)
+          (else (ascending? (cdr asc-lst)))
+    )
+)
 
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp)
+    (cond ((= exp 0) 1)
+        ((even? exp) (square (pow base (/ exp 2))))
+        (else (* base (pow base (- exp 1))))
+    )
+)

ac5ce3142df4848961c1cb1106be245e368b313c
zhenqihu <zhenqi_hu@outlook.com>
2023-04-14 01:52:23 +0800

    finish lab10

--- lab/lab10/lab10.scm
+++ lab/lab10/lab10.scm
@@ -1,9 +1,42 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+;(define (over-or-under num1 num2)
+;    (cond ((< num1 num2) -1)
+;          ((= num1 num2) 0)
+;          ((> num1 num2) 1)
+;    )
+;)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+    (if (< num1 num2) -1
+    (if (= num1 num2) 0 1)
+    )
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
 
-(define lst 'YOUR-CODE-HERE)
+(define (make-adder num)
+    (lambda (inc) (+ num inc))
+)
 
-(define (duplicate lst) 'YOUR-CODE-HERE)
+(define (composed f g) 
+    (lambda (x) (f (g x)))
+)
+
+(define lst 
+    ;(cons (cons 1 nil) (list 2 (list 3 4) 5))
+    (cons (cons 1 nil)
+        (cons 2
+            (cons (cons 3 (cons 4 nil))
+                (cons 5 nil)
+            )
+        )
+    )
+)
+
+(define (duplicate lst)
+    (if (null? lst) nil
+        (cons (car lst)
+            (cons (car lst)
+                (duplicate (cdr lst))
+            )
+        )
+    )
+)

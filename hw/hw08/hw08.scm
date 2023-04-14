ec7f0e2ad029419114f5f09dd82f77e6de4702c9
zhenqihu <zhenqi_hu@outlook.com>
2023-04-14 21:16:08 +0800

    finish hw08

--- hw/hw08/hw08.scm
+++ hw/hw08/hw08.scm
@@ -1,8 +1,31 @@
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s) 
+  (cond 
+    ((null? s) s)
+    ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+    (else (my-filter pred (cdr s)))
+  )
+)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) 
+  (cond
+    ((null? lst1) lst2)
+    ((null? lst2) lst1)
+    (else (cons (car lst1) (interleave lst2 (cdr lst1))))
+  )
+)
 
 (define (accumulate joiner start n term)
-  'YOUR-CODE-HERE)
+  (if (= n 0) start
+    (joiner (term n) (accumulate joiner start (- n 1) term))
+  )
+)
 
-(define (no-repeats lst) 'YOUR-CODE-HERE)
+(define (no-repeats lst)
+  (if (null? lst)
+    lst
+    (cons 
+      (car lst)
+      (no-repeats (my-filter (lambda (x) (not (= x (car lst)))) (cdr lst)))
+    )
+  )
+)

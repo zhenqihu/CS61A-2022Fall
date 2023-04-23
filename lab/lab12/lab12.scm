deddb9cfa228d69c15721d15f27a8a90af60db8e
zhenqihu <zhenqi_hu@outlook.com>
2023-04-23 14:32:29 +0800

    add hw 09

--- lab/lab12/lab12.scm
+++ lab/lab12/lab12.scm
@@ -1,20 +1,22 @@
 (define-macro
  (if-macro condition if-true if-false)
- 'YOUR-CODE-HERE)
+ (list 'if condition if-true if-false)
+)
 
 (define-macro (or-macro expr1 expr2)
-  `(let ((v1 ____________))
-     (if _____
-         _____
-         _____)))
+  `(let ((v1 ,expr1))
+     (if v1
+         v1
+         ,expr2)))
 
-(define (replicate x n) 'YOUR-CODE-HERE)
+(define (replicate x n) (if (= n 0) () (cons x (replicate x (- n 1)))))
 
-(define-macro (repeat-n expr n) 'YOUR-CODE-HERE)
+(define-macro (repeat-n expr n) (cons 'begin (replicate expr (eval n))))
 
 (define
  (list-of map-expr for var in lst if filter-expr)
- 'YOUR-CODE-HERE)
+ `(map (lambda (,var) ,map-expr) (filter (lambda (,var) ,filter-expr) ,lst))
+ )
 
 (define-macro (list-of-macro map-expr
                              for
@@ -23,4 +25,4 @@
                              lst
                              if
                              filter-expr)
-  'YOUR-CODE-HERE)
+  `(map (lambda (,var) ,map-expr) (filter (lambda (,var) ,filter-expr) ,lst)))

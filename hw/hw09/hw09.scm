ae9a9c39e3d681da709c440533fc68d5112e4235
zhenqihu <zhenqi_hu@outlook.com>
2023-04-23 16:08:20 +0800

    finish hw09

--- hw/hw09/hw09.scm
+++ hw/hw09/hw09.scm
@@ -1,8 +1,9 @@
 (define-macro (when condition exprs)
-  'YOUR-CODE-HERE)
+  `(if ,condition ,(cons 'begin exprs) (quote okay))
+  )
 
 (define-macro (switch expr cases)
-  (cons _________
-        (map (_________ (_________)
-                        (cons _________ (cdr case)))
+  (cons 'cond
+        (map (lambda (case)
+                        (cons `(equal? ,expr (quote ,(car case))) (cdr case)))
              cases)))
--- hw/hw09/tests/wwsd-eval_apply.py
+++ hw/hw09/tests/wwsd-eval_apply.py
@@ -5,7 +5,7 @@ test = {
     {
       'cases': [
         {
-          'answer': '17c904758d7c0462b49135eebe9c3ca4',
+          'answer': '6',
           'choices': [
             '1',
             '2',
@@ -15,12 +15,12 @@ test = {
             '8'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': 'How many calls are made to scheme_eval to evaluate the expression: (+ 2 4 6 8) ?'
         },
         {
-          'answer': 'd7ab3c9f4f7487833d3cb935fc8c712a',
+          'answer': '1',
           'choices': [
             '1',
             '2',
@@ -30,12 +30,12 @@ test = {
             '8'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': 'How many calls are made to scheme_apply: (+ 2 4 6 8) ?'
         },
         {
-          'answer': '1f7858498ce9c3b635426bdfd8df1078',
+          'answer': '10',
           'choices': [
             '3',
             '7',
@@ -45,12 +45,12 @@ test = {
             '13'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': 'How many calls are made to scheme_eval: (+ 2 (* 4 (- 6 8))) ?'
         },
         {
-          'answer': 'd17a76b23ca9a0f23c264eaf1fedfa70',
+          'answer': '3',
           'choices': [
             '3',
             '7',
@@ -60,12 +60,12 @@ test = {
             '13'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': 'How many calls are made to scheme_apply: (+ 2 (* 4 (- 6 8))) ?'
         },
         {
-          'answer': '17c904758d7c0462b49135eebe9c3ca4',
+          'answer': '6',
           'choices': [
             '1',
             '2',
@@ -75,12 +75,12 @@ test = {
             '10'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': 'How many calls are made to scheme_eval: (if #f (+ 2 3) (+ 1 2)) ?'
         },
         {
-          'answer': 'd7ab3c9f4f7487833d3cb935fc8c712a',
+          'answer': '1',
           'choices': [
             '1',
             '2',
@@ -90,12 +90,12 @@ test = {
             '10'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': 'How many calls are made to scheme_apply: (if #f (+ 2 3) (+ 1 2)) ?'
         },
         {
-          'answer': 'd7ab3c9f4f7487833d3cb935fc8c712a',
+          'answer': '1',
           'choices': [
             '0',
             '1',
@@ -105,12 +105,12 @@ test = {
             '9'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': 'How many calls are made to scheme_eval: (define (cube a) (* a a a)) ?'
         },
         {
-          'answer': '023f53b43f605b7580be5aa5c3e5ee7e',
+          'answer': '0',
           'choices': [
             '0',
             '1',
@@ -120,12 +120,12 @@ test = {
             '9'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': 'How many calls are made to scheme_apply: (define (cube a) (* a a a)) ?'
         },
         {
-          'answer': '5ccd251f8efaf1413d8d9a6429728bd6',
+          'answer': '8',
           'choices': [
             '2',
             '3',
@@ -135,12 +135,12 @@ test = {
             '11'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': 'How many calls are made to scheme_eval: (cube 3) ?'
         },
         {
-          'answer': '3940351fe1ecdc23ea60a8fdad9aa11d',
+          'answer': '2',
           'choices': [
             '2',
             '3',
@@ -150,7 +150,7 @@ test = {
             '11'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': 'How many calls are made to scheme_apply: (cube 3) ?'
         }
